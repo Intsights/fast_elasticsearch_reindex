688546b16c70eb2871899f7c413582659265498f
Adir Haleli <adir@intsights.com>
2020-10-08 14:54:37 +0300

    --wip-- [skip ci]

--- setup.py
+++ setup.py
@@ -3,7 +3,12 @@ import setuptools
 
 setuptools.setup(
     name='fast_elasticsearch_reindex',
+    description="A Python based alternative to Elasticsearch Reindex API with multiprocessing support",
     version='1.0.0',
+    author="Adir Haleli",
+    author_email="adir544@gmail.com",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
     install_requires=[
         'elasticsearch',
@@ -11,4 +16,11 @@ setuptools.setup(
         'tenacity',
         'tqdm',
     ],
+    url="https://github.com/Intsights/fast_elasticsearch_reindex",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.4',
 )
